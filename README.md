6a62d5ba4be24581d7b1fea02aa826c13471814c
Margaret Janiczek <margie.hannum@gmail.com>
2022-12-01 14:46:33 -0500

    moved files from diffnettutorial to here

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ssjgl-tutorial
 Tutorial for using Bayesian joint spike-and-slab graphical lasso in R
+
+This tutorial summarizes details of the Bayesian Joint spike-and-slab Graphical Lasso (SSJGL) algorithm (Li et. al 2019), then walks through the steps needed to run the `spikeyglass` package using a real-world metabolomics dataset.
+
+To view the tutorial click [here](https://mljaniczek.github.io/ssjgl-tutorial). 
+
+To run this tutorial yourself, I recommend cloning [the repository](https://github.com/mljaniczek/ssjgl-tutorial) (click the green "CODE" button, then download the zip or open with GitHub Desktop), then opening the file `ssjgl-tutorial.Rproj` and running the code within that environment. Alternatively you can download the .csv file and .Rmd file and run them yourself. 
+
+Ensure you have installed all the necessary packages to run this tutorial!
+
+```
+#install.packages("JGL")
+#install.packages("igraph", "RColorBrewer", "pheatmap")
+#install.packages("tidyverse", "here")
+#devtools::install_github("mljaniczek/spikeyglass")
+```
---
+++ helpers.R
@@ -0,0 +1,72 @@
+plot_jgl <- function(
+    thisGraph,
+    multiplier = 15,
+    vertex.size = 4,  
+    vertex.label.dist = 2 , 
+    vertex.label.cex = 0.5,
+    rescale = F,
+    vertex.color = NULL, 
+    vertex.label.color= NULL, 
+    main = NULL,
+    #sub = NULL,
+    ...) {
+  myLayout =  layout_in_circle(makePrettyGraphFromGraph(thisGraph))*0.8
+  lab.locs <- radian.rescale(x=1:length(V(thisGraph)), direction=-1, start=0)
+  plot(makePrettyGraphFromGraph(thisGraph, multiplier = multiplier), 
+       vertex.size = vertex.size,  
+       vertex.label.dist = vertex.label.dist , 
+       vertex.label.cex = vertex.label.cex, 
+       layout = myLayout,
+       lab.locs = lab.locs,
+       vertex.label.degree=lab.locs,
+       vertex.color = vertex.color,
+       vertex.label.color= vertex.label.color,
+       rescale=F, 
+       main = main,
+       #sub = sub,
+       ...)
+}
+
+
+# this is a function for making red-blue edges to match the edge weights(partial correlation)
+# you may need to adjust the multiplier depending on how strong your edges are
+# function from Kate and Yukun
+makePrettyGraphFromGraph = function(thisGraph, multiplier = 15,redblue=T)
+{
+  if(redblue == T) my_palette <- brewer.pal(n = 8, name = "RdBu")
+  if(redblue == F) my_palette <- brewer.pal(n = 8, name = "PRGn")
+  my_palette <- my_palette[8:1]
+  E(thisGraph)$width = abs(E(thisGraph)$weight)*multiplier
+  E(thisGraph)$color = ifelse(sign(E(thisGraph)$weight)>0,my_palette[1],my_palette[8])
+  return(thisGraph)
+}
+
+
+# this is code from this stack exchange: 
+# https://stackoverflow.com/questions/23209802/placing-vertex-label-outside-a-circular-layout-in-igraph
+
+radian.rescale <- function(x, start=0, direction=1) {
+  c.rotate <- function(x) (x + start) %% (2 * pi) * direction
+  c.rotate(scales::rescale(x, c(0, 2 * pi), range(x)))
+}
+
+# this function from Koyejo Lab Github https://github.com/koyejo-lab/JointGraphicalLasso
+# input is list of matrices same as you would put into `JGL` function, as well as results of `JGL`
+# output is AIC for particular estimated graph.
+# use this function when tuning lambdas
+vBIC <- function(X, est_graph, thr=0.001){
+  num <- length(X)
+  BIC_acc <- 0.
+  for(i in 1:num){
+    
+    data_num <- dim(X[[i]])[1]
+    sample_cov <- cov(X[[i]], X[[i]])
+    tr_sum <- sum(diag(sample_cov %*% est_graph$theta[[i]]))
+    
+    log_det <- determinant(est_graph$theta[[i]], logarithm = TRUE)$modulus[1][1]
+    
+    E <- sum(sum(abs(est_graph$theta[[i]]) >= thr))
+    BIC_acc <- BIC_acc + (tr_sum - log_det) + (log(data_num)*E/data_num)
+  }
+  return(BIC_acc)
+}
